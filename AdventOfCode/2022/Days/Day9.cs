4b0500b720a38d2d49067f5a59d5e3bd4df0989b
ZachStolte <zrs9346@g.rit.edu>
2023-01-03 13:53:49 -0500

    Day9 initial

---
+++ AdventOfCode/2022/Days/Day9.cs
@@ -0,0 +1,26 @@
+  
+  
+  class Day9
+    {
+        public static void Part1(StreamReader sr)
+        {
+            string line = "";
+            line = sr.ReadLine();
+            while (line!=null){
+
+                line = sr.ReadLine();
+            }     
+
+        }
+
+        public static void Part2(StreamReader sr)
+        {
+            string line = "";
+            line = sr.ReadLine();
+            while (line!=null){
+
+                line = sr.ReadLine();
+            }     
+
+        }
+    }
