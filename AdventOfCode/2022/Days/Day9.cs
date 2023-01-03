32bfd5e1420cfe98d207d2f4ee044cbe3dac30eb
ZachStolte <zrs9346@g.rit.edu>
2023-01-03 16:22:31 -0500

    Day 9 Completed
    Spent nearly an hour on an issue with Part 1, issue was just that I would only read the first digit of a number command. Part 2 took sub 5 minutes.

--- AdventOfCode/2022/Days/Day9.cs
+++ AdventOfCode/2022/Days/Day9.cs
@@ -40,6 +40,7 @@
         public static void Part1(StreamReader sr)
         {
             string line = "";
+            int temp = 0;
             line = sr.ReadLine();
             List<int> headPos = new List<int>();
             headPos.Add(0);
@@ -53,9 +54,10 @@
             initialPos.Add(0);
             visitedPos.Add(initialPos);
             while (line!=null){
-                for (int i = 0; i < Int32.Parse(line[2].ToString()); i++){
+                String number = line.Split(" ")[1];
+                for (int i = 0; i < Int32.Parse(number); i++){
                     if(line[0] == 'U'){
-                    headPos[0] ++;
+                        headPos[0] ++;
                     }
                     else if(line[0] == 'R'){
                         headPos[1] ++;
@@ -90,10 +92,55 @@
         {
             string line = "";
             line = sr.ReadLine();
+            List<int> headPos = new List<int>();
+            headPos.Add(0);
+            headPos.Add(0);
+            List<List<int>> tailsPos = new List<List<int>>();
+            for(int i = 0; i < 9; i++){
+                tailsPos.Add(new List<int>());
+                tailsPos[i].Add(0);
+                tailsPos[i].Add(0);
+            }
+            List<List<int>> visitedPos = new List<List<int>>();
+            List<int> initialPos = new List<int>();
+            initialPos.Add(0);
+            initialPos.Add(0);
+            visitedPos.Add(initialPos);
             while (line!=null){
-
+                String number = line.Split(" ")[1];
+                for (int i = 0; i < Int32.Parse(number); i++){
+                    if(line[0] == 'U'){
+                        headPos[0] ++;
+                    }
+                    else if(line[0] == 'R'){
+                        headPos[1] ++;
+                    }
+                    else if(line[0] == 'D'){
+                        headPos[0] --;
+                    }
+                    else if(line[0] == 'L'){
+                        headPos[1] --;
+                    }
+                    tailMove(headPos, tailsPos[0]);
+                    for (int j = 1; j < 9; j++){
+                        tailMove(tailsPos[j-1], tailsPos[j]);
+                    }
+                    Boolean unique = true;
+                    for (int j = 0; j < visitedPos.Count(); j++){
+                        if(tailsPos[8][0] == visitedPos[j][0] && tailsPos[8][1] == visitedPos[j][1]){
+                            unique = false;
+                        }
+                    }
+                    if (unique){
+                        List<int> tempPos = new List<int>();
+                        tempPos.Add(tailsPos[8][0]);
+                        tempPos.Add(tailsPos[8][1]);
+                        visitedPos.Add(tempPos);
+                    }
+                }
                 line = sr.ReadLine();
-            }     
+            }
+            Console.Write(visitedPos.Count());     
 
         }
     }
