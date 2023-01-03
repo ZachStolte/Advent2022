f99f66e5369b5ada432428efb8e1544ee080f2bc
ZachStolte <zrs9346@g.rit.edu>
2023-01-03 16:56:28 -0500

    Day 10 Completed
    Not too bad. Part two was very tough to understand the directions of, but not difficult to implement.

---
+++ AdventOfCode/2022/Days/Day10.cs
@@ -0,0 +1,80 @@
+  class Day10
+    {
+        public static void Part1(StreamReader sr)
+        {
+            string line = "";
+            int sigStrength = 1;
+            int finalStrength = 0;
+            int cycle = 0;
+            line = sr.ReadLine();
+            while (line!=null){
+                if ((cycle-20)%40 == 0){
+                    finalStrength+=cycle*sigStrength;
+                }
+                string[] splitter = line.Split(" ");
+                if (splitter[0] == "addx"){
+                    cycle++;
+                    if ((cycle-20)%40 == 0){
+                        finalStrength+=cycle*sigStrength;
+                    }
+                    cycle++;
+                    sigStrength += Int32.Parse(splitter[1]);
+                }
+                else if (splitter[0] == "noop"){
+                    cycle++;
+                }
+                line = sr.ReadLine();
+            }
+
+            Console.Write(finalStrength);     
+
+        }
+
+        public static void Part2(StreamReader sr)
+        {
+            string line = "";
+            int register = 1;
+            int pixelPos = 0;
+            int cycle = 0;
+            List<string> image = new List<string>();
+            line = sr.ReadLine();
+            while (line!=null){
+                if (cycle == register || cycle == register-1 || cycle == register+1){
+                    image.Add("#");
+                }
+                else{
+                    image.Add(".");
+                }
+                if ((cycle+1)%40 == 0){
+                    image.Add("\n");
+                    cycle -= 40;
+                }
+                string[] splitter = line.Split(" ");
+                if (splitter[0] == "addx"){
+                    cycle++;
+                    if (cycle == register || cycle == register-1 || cycle == register+1){
+                        image.Add("#");
+                    }
+                    else{
+                        image.Add(".");
+                    }
+                    if ((cycle+1)%40 == 0){
+                        image.Add("\n");
+                        cycle-=40;
+                    }
+                    cycle++;
+                    register += Int32.Parse(splitter[1]);
+                }
+                else if (splitter[0] == "noop"){
+                    cycle++;
+
+                }
+                line = sr.ReadLine();
+            }
+
+            for(int i = 0; i < image.Count(); i++){
+                Console.Write(image[i]);
+            }      
+
+        }
+    }
